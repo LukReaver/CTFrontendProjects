550df96dc836c50d295413d790baf3e2482d1476
LukReaver <l88uka@gmail.com>
2018-09-27 11:16:15 +0200

    media queries / js features

--- WybieramEs/src/js/app.js
+++ WybieramEs/src/js/app.js
@@ -1,6 +1,41 @@
 
 $(document).ready(function() {
  
-
-
-});
+  $('.navbar-toggler').on('click', function () {  
+   $('.animated-icon0').toggleClass('open');  
+ });
+ 
+   $("ul.nav li.dropdown").hover(
+     function() {
+       $(this)
+         .find(".dropdown-menu")
+         .stop(true, true)
+         // .delay(100)
+         .fadeIn(200);
+     },
+     function() {
+       $(this)
+         .find(".dropdown-menu")
+         .stop(true, true)
+         .delay(100)
+         .fadeOut(200);
+     }
+   ); 
+   
+   $("ul.nav li.dropdown").hover(
+     function() {
+       $(this)
+         .find(".fas")             
+         .css({"-webkit-transform-origin":"50% 70%"})
+         .css({"-webkit-transform":" rotate(180deg)"})        
+     },
+     function() {
+       $(this)
+       .find(".fas")      
+         .css({"-webkit-transform":" rotate(0deg)"})        
+     }
+   ); 
+ 
+ 
+ });
+ 
